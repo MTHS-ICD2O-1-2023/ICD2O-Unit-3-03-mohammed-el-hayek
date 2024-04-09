7f818e8bd05d6b59afe1c2a1d5d3c31888b138bf
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-09 04:12:59 +0000

    update 1.0v

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohmmed El-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohmmed El-hayek
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a pyramid
+ */
+function calculate () {
+  // input
+  const radius = parseInt(document.getElementById('Radius for the Sphere').value)
+
+  // process
+  const volume = 4/3 * Math.PI * radius ** 3
+
+  // output
+  document.getElementById('Volume').innerHTML = `the volume is ${volume.toFixed(1)}`
+}
